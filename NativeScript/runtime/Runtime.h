f1adc766957f0b748237d54c7967ce0e492ac69e
⭐caitp⭐ <caitp@users.noreply.github.com>
2023-05-29 09:35:11 -0400

    Garbage changes to revert

--- NativeScript/runtime/Runtime.h
+++ NativeScript/runtime/Runtime.h
@@ -11,6 +11,11 @@
 
 namespace tns {
 
+static constexpr int kEmbedderNativeScript = 0x6E53;
+
+class BaseDataWrapper;
+class WorkerHolder;
+class WorkerHolderData;
 class Runtime {
 public:
     Runtime();
